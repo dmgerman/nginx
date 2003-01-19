dc77543a01d283d7e69f25f027daf5fd7a7dd056
Igor Sysoev <igor@sysoev.ru>
2003-01-19 17:53:14 +0000

    nginx-0.0.1-2003-01-19-20:53:14 import
    Former-commit-id: de8fcead5e89f4139f1dbfe1407aac8ba4ada333

--- src/http/modules/ngx_http_index_handler.c
+++ src/http/modules/ngx_http_index_handler.c
@@ -248,7 +248,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_comment
-comment|/*    Try to open first index file before the test of the directory existence    because the valid requests should be many more then invalid ones.    If open() is failed then stat() should be more quickly because some data    is already cached in the kernel.  Besides Win32 has ERROR_PATH_NOT_FOUND    and Unix has ENOTDIR error (although it less helpfull). */
+comment|/*    Try to open first index file before the test of the directory existence    because the valid requests should be many more then invalid ones.    If open() failed then stat() should be more quickly because some data    is already cached in the kernel.  Besides Win32 has ERROR_PATH_NOT_FOUND    and Unix has ENOTDIR error (although it less helpfull). */
 end_comment
 
 begin_function
