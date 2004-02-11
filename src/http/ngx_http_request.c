1da3f0e05d21a2f57178cfe0462f2fb90533b2c6
Igor Sysoev <igor@sysoev.ru>
2004-02-11 07:19:26 +0000

    nginx-0.0.2-2004-02-11-10:19:26 import
    Former-commit-id: dfe63ad1833a8d6d9398371ba595a1d8a19d6148

--- src/http/ngx_http_request.c
+++ src/http/ngx_http_request.c
@@ -449,7 +449,7 @@ name|event_handler
 operator|=
 name|ngx_http_init_request
 expr_stmt|;
-comment|/* STUB: epoll edge */
+comment|/* STUB: epoll */
 name|c
 operator|->
 name|write
@@ -5372,18 +5372,6 @@ name|c
 operator|->
 name|buffer
 expr_stmt|;
-name|wev
-operator|=
-name|c
-operator|->
-name|write
-expr_stmt|;
-name|wev
-operator|->
-name|event_handler
-operator|=
-name|ngx_http_empty_handler
-expr_stmt|;
 if|if
 condition|(
 name|h
@@ -5510,6 +5498,18 @@ operator|->
 name|event_handler
 operator|=
 name|ngx_http_keepalive_handler
+expr_stmt|;
+name|wev
+operator|=
+name|c
+operator|->
+name|write
+expr_stmt|;
+name|wev
+operator|->
+name|event_handler
+operator|=
+name|ngx_http_empty_handler
 expr_stmt|;
 if|if
 condition|(
