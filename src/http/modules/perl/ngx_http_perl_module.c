39d705a2403bc263e4fb6e2df4224ee9f93293b1
Igor Sysoev <igor@sysoev.ru>
2006-11-26 16:56:41 +0000

    add comment
    Former-commit-id: 9a29d129f20873f4de4553eab00b869815a75bac

--- src/http/modules/perl/ngx_http_perl_module.c
+++ src/http/modules/perl/ngx_http_perl_module.c
@@ -28,7 +28,7 @@ file|<ngx_http_perl_module.h>
 end_include
 
 begin_typedef
-DECL|struct|__anon2c1995780108
+DECL|struct|__anon2c5b271e0108
 typedef|typedef
 struct|struct
 block|{
@@ -57,7 +57,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon2c1995780208
+DECL|struct|__anon2c5b271e0208
 typedef|typedef
 struct|struct
 block|{
@@ -77,7 +77,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon2c1995780308
+DECL|struct|__anon2c5b271e0308
 typedef|typedef
 struct|struct
 block|{
@@ -97,7 +97,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon2c1995780408
+DECL|struct|__anon2c5b271e0408
 typedef|typedef
 struct|struct
 block|{
@@ -2856,6 +2856,7 @@ argument_list|,
 name|FALSE
 argument_list|)
 expr_stmt|;
+comment|/* eval_pv() does not set ERRSV on failure */
 return|return;
 block|}
 operator|*
