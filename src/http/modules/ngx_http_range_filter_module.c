62174a34eca2f3ccdba7b66b142f176cc0f02219
Igor Sysoev <igor@sysoev.ru>
2006-12-28 20:12:03 +0000

    style fix
    Former-commit-id: c49d3ec7d4ff3332bcf6838e83af058988e99eee

--- src/http/modules/ngx_http_range_filter_module.c
+++ src/http/modules/ngx_http_range_filter_module.c
@@ -26,7 +26,7 @@ comment|/*  * the single part format:  *  * "HTTP/1.0 206 Partial Content" CRLF 
 end_comment
 
 begin_typedef
-DECL|struct|__anon28ec41e90108
+DECL|struct|__anon278c4d880108
 typedef|typedef
 struct|struct
 block|{
