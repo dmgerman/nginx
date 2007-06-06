dmgerman/nginx

commit 83a070d46c7672a8dbc5481bc7fc0da7b121b811

inherit flush flag, this fixes https proxying
git-svn-id: svn://svn.nginx.org/nginx/trunk@1228 c3fe7df1-7212-e011-8a91-001109144009

src/core/ngx_output_chain.c
@@ -377,8 +377,9 @@ ngx_output_chain_copy_buf(ngx_buf_t *dst, ngx_buf_t *src, ngx_uint_t sendfile)
             dst->in_file = 0;
         }
 
-        if (src->last_buf && src->pos == src->last) {
-            dst->last_buf = 1;
+        if (src->pos == src->last) {
+            dst->flush = src->flush;
+            dst->last_buf = src->last_buf;
         }
 
     } else {
@@ -417,8 +418,9 @@ ngx_output_chain_copy_buf(ngx_buf_t *dst, ngx_buf_t *src, ngx_uint_t sendfile)
 
         src->file_pos += n;
 
-        if (src->last_buf && src->file_pos == src->file_last) {
-            dst->last_buf = 1;
+        if (src->pos == src->last) {
+            dst->flush = src->flush;
+            dst->last_buf = src->last_buf;
         }
     }
 

commit 07d7d0c8187089941f51612b4d98addb18d87d64

fix bug introduced in r1228
Former-commit-id: 52a93439bc737ef6ad7ac99b329ebdbcca6d31c3

src/core/ngx_output_chain.c
@@ -1678,11 +1678,11 @@ if|if
 condition|(
 name|src
 operator|->
-name|pos
+name|file_pos
 operator|==
 name|src
 operator|->
-name|last
+name|file_last
 condition|)
 block|{
 name|dst