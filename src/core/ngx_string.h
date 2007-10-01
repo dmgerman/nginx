58b37bf36c297b8932b2d782935ea67b008e9959
Igor Sysoev <igor@sysoev.ru>
2007-10-01 14:48:33 +0000

    ngx_strnstr()
    Former-commit-id: 35fe5fd06dd1307e1e3cc8a0212e19be0e04b58c

--- src/core/ngx_string.h
+++ src/core/ngx_string.h
@@ -29,7 +29,7 @@ file|<ngx_core.h>
 end_include
 
 begin_typedef
-DECL|struct|__anon29ac3fdf0108
+DECL|struct|__anon294461a60108
 typedef|typedef
 struct|struct
 block|{
@@ -49,7 +49,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29ac3fdf0208
+DECL|struct|__anon294461a60208
 typedef|typedef
 struct|struct
 block|{
@@ -68,7 +68,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon29ac3fdf0308
+DECL|struct|__anon294461a60308
 typedef|typedef
 struct|struct
 block|{
@@ -568,6 +568,25 @@ modifier|*
 name|s1
 parameter_list|,
 name|u_char
+modifier|*
+name|s2
+parameter_list|,
+name|size_t
+name|n
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|u_char
+modifier|*
+name|ngx_strnstr
+parameter_list|(
+name|u_char
+modifier|*
+name|s1
+parameter_list|,
+name|char
 modifier|*
 name|s2
 parameter_list|,
