dmgerman/nginx

commit 282b1efd4bd276f8c3989cc5a5af21e042e8c146

delete code that should be deleted in r1576
git-svn-id: svn://svn.nginx.org/nginx/trunk@1578 c3fe7df1-7212-e011-8a91-001109144009

src/os/unix/ngx_process_cycle.c
@@ -1053,17 +1053,7 @@ ngx_channel_handler(ngx_event_t *ev)
     ngx_log_debug1(NGX_LOG_DEBUG_CORE, ev->log, 0, "channel: %i", n);
 
     if (n == NGX_ERROR) {
-
         ngx_close_connection(c);
-
-        fd = c->fd;
-        c->fd = (ngx_socket_t) -1;
-
-        if (close(fd) == -1) {
-            ngx_log_error(NGX_LOG_ALERT, ev->log, ngx_errno,
-                          "close() channel failed");
-        }
-
         return;
     }
 

commit 0ced46636b41d038ba63f9a5a0b9367707de1af6

fix building broken in r1578
Former-commit-id: 35ab2cb3731caa03530ff9754089e7007d708398

src/os/unix/ngx_process_cycle.c
@@ -4644,9 +4644,6 @@ block|{
 name|ngx_int_t
 name|n
 decl_stmt|;
-name|ngx_socket_t
-name|fd
-decl_stmt|;
 name|ngx_channel_t
 name|ch
 decl_stmt|;