dac1789450dd989c22f59f3daf5828726042f288
Igor Sysoev <igor@sysoev.ru>
2007-11-23 17:27:01 +0000

    fix previous commit
    Former-commit-id: 53fcff1efebea2103c8851c6a040af04d3f22c30

--- src/core/ngx_core.h
+++ src/core/ngx_core.h
@@ -445,6 +445,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<ngx_resolver.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<ngx_open_file_cache.h>
 end_include
 
