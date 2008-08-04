dmgerman/nginx

commit c9efb2d11c484451c79753af2ccd2f9b0a974c51

directio
git-svn-id: svn://svn.nginx.org/nginx/trunk@2130 c3fe7df1-7212-e011-8a91-001109144009

src/os/win32/ngx_files.h
@@ -200,7 +200,7 @@ typedef struct {
 
 
 ngx_int_t ngx_open_glob(ngx_glob_t *gl);
-#define ngx_open_glob_n          "FindFirstFile()"
+#define ngx_open_glob_n             "FindFirstFile()"
 
 ngx_int_t ngx_read_glob(ngx_glob_t *gl, ngx_str_t *name);
 void ngx_close_glob(ngx_glob_t *gl);
@@ -214,6 +214,10 @@ ssize_t ngx_write_file(ngx_file_t *file, u_char *buf, size_t size,
 
 ssize_t ngx_write_chain_to_file(ngx_file_t *file, ngx_chain_t *ce,
     off_t offset, ngx_pool_t *pool);
+
+
+#define ngx_directio(fd)            0
+#define ngx_directio_n              "ngx_directio_n"
 
 
 #endif /* _NGX_FILES_H_INCLUDED_ */

src/os/unix/ngx_files.c
@@ -351,3 +351,22 @@ ngx_unlock_fd(ngx_fd_t fd)
 
     return 0;
 }
+
+
+#if (NGX_HAVE_O_DIRECT)
+
+ngx_int_t
+ngx_directio(ngx_fd_t fd)
+{
+    int  flags;
+
+    flags = fcntl(fd, F_GETFL);
+
+    if (flags == -1) {
+        return -1;
+    }
+
+    return fcntl(fd, F_SETFL, flags | O_DIRECT);
+}
+
+#endif

src/os/unix/ngx_files.h
@@ -189,4 +189,22 @@ ngx_err_t ngx_unlock_fd(ngx_fd_t fd);
 #define ngx_unlock_fd_n          "fcntl(F_SETLK, F_UNLCK)"
 
 
+#if (NGX_HAVE_O_DIRECT)
+
+ngx_int_t ngx_directio(ngx_fd_t fd);
+#define ngx_directio_n           "fcntl(O_DIRECT)"
+
+#elif (NGX_HAVE_F_NOCACHE)
+
+#define ngx_directio(fd)         fcntl(fd, F_NOCACHE, 1)
+#define ngx_directio_n           "fcntl(F_NOCACHE)"
+
+#else
+
+#define ngx_directio(fd)         0
+#define ngx_directio_n           "ngx_directio_n"
+
+#endif
+
+
 #endif /* _NGX_FILES_H_INCLUDED_ */

auto/os/features
@@ -170,3 +170,23 @@ if [ $ngx_found = no ]; then
         CRYPT_LIB="-lcrypt"
     fi
 fi
+
+
+ngx_feature="O_DIRECT"
+ngx_feature_name="NGX_HAVE_O_DIRECT"
+ngx_feature_run=no
+ngx_feature_incs="#include <fcntl.h>"
+ngx_feature_path=
+ngx_feature_libs=
+ngx_feature_test="fcntl(0, F_SETFL, O_DIRECT);"
+. auto/feature
+
+
+ngx_feature="F_NOCACHE"
+ngx_feature_name="NGX_HAVE_F_NOCACHE"
+ngx_feature_run=no
+ngx_feature_incs="#include <fcntl.h>"
+ngx_feature_path=
+ngx_feature_libs=
+ngx_feature_test="fcntl(0, F_NOCACHE, 1);"
+. auto/feature

src/core/ngx_open_file_cache.c
@@ -497,6 +497,13 @@ ngx_open_and_stat_file(u_char *name, ngx_open_file_info_t *of, ngx_log_t *log)
 
     } else {
         of->fd = fd;
+
+        if (of->directio <= ngx_file_size(&fi)) {
+            if (ngx_directio(fd) == -1) {
+                ngx_log_error(NGX_LOG_ALERT, log, ngx_errno,
+                              ngx_directio_n " \"%s\" failed", name);
+            }
+        }
     }
 
 done:

src/core/ngx_open_file_cache.h
@@ -17,6 +17,7 @@ typedef struct {
     ngx_file_uniq_t          uniq;
     time_t                   mtime;
     off_t                    size;
+    off_t                    directio;
     ngx_err_t                err;
 
     time_t                   valid;

src/core/ngx_output_chain.c
@@ -32,6 +32,7 @@ ngx_output_chain(ngx_output_chain_ctx_t *ctx, ngx_chain_t *in)
     size_t        size;
     ngx_int_t     rc, last;
     ngx_uint_t    recycled;
+    ngx_buf_t    *b;
     ngx_chain_t  *cl, *out, **last_out;
 
     if (ctx->in == NULL && ctx->busy == NULL) {
@@ -161,13 +162,29 @@ ngx_output_chain(ngx_output_chain_ctx_t *ctx, ngx_chain_t *in)
                         }
                     }
 
-                    ctx->buf = ngx_create_temp_buf(ctx->pool, size);
-                    if (ctx->buf == NULL) {
+                    b = ngx_calloc_buf(ctx->pool);
+                    if (b == NULL) {
                         return NGX_ERROR;
                     }
 
-                    ctx->buf->tag = ctx->tag;
-                    ctx->buf->recycled = recycled;
+                    /*
+                     * allocate block aligned to a disk sector size
+                     * to enable O_DIRECT
+                     */
+
+                    b->start = ngx_pmemalign(ctx->pool, size, 512);
+                    if (b->start == NULL) {
+                        return NGX_ERROR;
+                    }
+
+                    b->pos = b->start;
+                    b->last = b->start;
+                    b->end = b->last + size;
+                    b->temporary = 1;
+                    b->tag = ctx->tag;
+                    b->recycled = recycled;
+
+                    ctx->buf = b;
                     ctx->allocated++;
                 }
             }

src/core/ngx_palloc.c
@@ -195,17 +195,35 @@ ngx_palloc_large(ngx_pool_t *pool, size_t size)
     void              *p;
     ngx_pool_large_t  *large;
 
-#if 0
-    p = ngx_memalign(ngx_pagesize, size, pool->log);
-    if (p == NULL) {
-        return NULL;
-    }
-#else
     p = ngx_alloc(size, pool->log);
     if (p == NULL) {
         return NULL;
     }
-#endif
+
+    large = ngx_palloc(pool, sizeof(ngx_pool_large_t));
+    if (large == NULL) {
+        ngx_free(p);
+        return NULL;
+    }
+
+    large->alloc = p;
+    large->next = pool->large;
+    pool->large = large;
+
+    return p;
+}
+
+
+void *
+ngx_pmemalign(ngx_pool_t *pool, size_t size, size_t alignment)
+{
+    void              *p;
+    ngx_pool_large_t  *large;
+
+    p = ngx_memalign(alignment, size, pool->log);
+    if (p == NULL) {
+        return NULL;
+    }
 
     large = ngx_palloc(pool, sizeof(ngx_pool_large_t));
     if (large == NULL) {

commit 18207e533dfd63cc8d34c8c8da009b1eb403ae74

fix building by bcc broken in r2130
Former-commit-id: eb3e4c8be00d6353b5e545a75f9abda0b154585c

src/os/win32/ngx_files.c
@@ -1863,5 +1863,20 @@ return|;
 block|}
 end_function
 
+begin_function
+name|ngx_int_t
+DECL|function|ngx_directio (ngx_fd_t fd)
+name|ngx_directio
+parameter_list|(
+name|ngx_fd_t
+name|fd
+parameter_list|)
+block|{
+return|return
+literal|0
+return|;
+block|}
+end_function
+
 end_unit
 

src/os/win32/ngx_files.h
@@ -840,7 +840,7 @@ value|(time_t) (((((unsigned __int64)                                          \
 end_define
 
 begin_typedef
-DECL|struct|__anon2bdaca610108
+DECL|struct|__anon2a19f6700108
 typedef|typedef
 struct|struct
 block|{
@@ -1011,16 +1011,15 @@ parameter_list|)
 function_decl|;
 end_function_decl
 
-begin_define
-DECL|macro|ngx_directio (fd)
-define|#
-directive|define
+begin_function_decl
+name|ngx_int_t
 name|ngx_directio
 parameter_list|(
+name|ngx_fd_t
 name|fd
 parameter_list|)
-value|0
-end_define
+function_decl|;
+end_function_decl
 
 begin_define
 DECL|macro|ngx_directio_n