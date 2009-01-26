dmgerman/nginx

commit 31b4544e3a68c0275a07e1cb9aa162a0a4d4227b

set the error level as default http error_log level
git-svn-id: svn://svn.nginx.org/nginx/trunk@2447 c3fe7df1-7212-e011-8a91-001109144009

src/core/ngx_cycle.c
@@ -394,10 +394,6 @@ ngx_init_cycle(ngx_cycle_t *old_cycle)
     cycle->log = cycle->new_log;
     pool->log = cycle->new_log;
 
-    if (cycle->log->log_level == 0) {
-        cycle->log->log_level = NGX_LOG_ERR;
-    }
-
 
     /* create shared memory */
 

src/core/ngx_log.c
@@ -302,7 +302,10 @@ ngx_set_error_log_levels(ngx_conf_t *cf, ngx_log_t *log)
         }
     }
 
-    if (log->log_level == NGX_LOG_DEBUG) {
+    if (log->log_level == 0) {
+        log->log_level = NGX_LOG_ERR;
+
+    } else if (log->log_level == NGX_LOG_DEBUG) {
         log->log_level = NGX_LOG_DEBUG_ALL;
     }
 

commit 1043d91e6415424bf8366c42bfb006ea45091f34

default error_log has zero level, the bug has been introduced in r2447
Former-commit-id: 71f729c78cb23c58adb62cb139f22a11b0c80ca0

src/core/ngx_cycle.c
@@ -864,6 +864,14 @@ name|cycle
 operator|->
 name|new_log
 operator|->
+name|log_level
+operator|=
+name|NGX_LOG_ERR
+expr_stmt|;
+name|cycle
+operator|->
+name|new_log
+operator|->
 name|file
 operator|->
 name|name