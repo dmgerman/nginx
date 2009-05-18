4ea018127127c0683cfe8763cf2b7d2ee1cfaef3
Igor Sysoev <igor@sysoev.ru>
2009-05-18 16:48:53 +0000

    bump version
    Former-commit-id: 6091c8c33ea8ea5dffa6ebb96b7d0715c2c32cbf

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|7058
+value|7059
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"0.7.58"
+value|"0.7.59"
 end_define
 
 begin_define
