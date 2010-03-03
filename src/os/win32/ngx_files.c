dmgerman/nginx

commit bdbdc02a2eca7268e7d4f95d5f2acc23edaf99a0

disable Win32 short file names
git-svn-id: svn://svn.nginx.org/nginx/trunk@3418 c3fe7df1-7212-e011-8a91-001109144009

src/os/win32/ngx_files.c
@@ -10,7 +10,7 @@
 
 #define NGX_UTF16_BUFLEN  256
 
-static u_short *ngx_utf8_to_utf16(u_short *utf16, u_char *utf8, size_t len);
+static u_short *ngx_utf8_to_utf16(u_short *utf16, u_char *utf8, size_t *len);
 
 
 /* FILE_FLAG_BACKUP_SEMANTICS allows to obtain a handle to a directory */
@@ -18,26 +18,59 @@ static u_short *ngx_utf8_to_utf16(u_short *utf16, u_char *utf8, size_t len);
 ngx_fd_t
 ngx_open_file(u_char *name, u_long mode, u_long create, u_long access)
 {
-    u_short    *u;
+    size_t      len;
+    u_long      n;
+    u_short    *u, *lu;
     ngx_fd_t    fd;
     ngx_err_t   err;
     u_short     utf16[NGX_UTF16_BUFLEN];
 
-    u = ngx_utf8_to_utf16(utf16, name, NGX_UTF16_BUFLEN);
+    len = NGX_UTF16_BUFLEN;
+    u = ngx_utf8_to_utf16(utf16, name, &len);
 
     if (u == NULL) {
         return INVALID_HANDLE_VALUE;
+    }
+
+    fd = INVALID_HANDLE_VALUE;
+    lu = NULL;
+
+    if (create == NGX_FILE_OPEN) {
+
+        lu = malloc(len * 2);
+        if (lu == NULL) {
+            goto failed;
+        }
+
+        n = GetLongPathNameW(u, lu, len);
+
+        if (n == 0) {
+            goto failed;
+        }
+
+        if (n != len - 1 || ngx_memcmp(u, lu, n) != 0) {
+            ngx_set_errno(NGX_ENOENT);
+            goto failed;
+        }
     }
 
     fd = CreateFileW(u, mode,
                      FILE_SHARE_READ|FILE_SHARE_WRITE|FILE_SHARE_DELETE,
                      NULL, create, FILE_FLAG_BACKUP_SEMANTICS, NULL);
 
-    if (u != utf16) {
-        err = ngx_errno;
-        ngx_free(u);
-        ngx_set_errno(err);
+failed:
+
+    err = ngx_errno;
+
+    if (lu) {
+        ngx_free(lu);
     }
+
+    if (u != utf16) {
+        ngx_free(u);
+    }
+
+    ngx_set_errno(err);
 
     return fd;
 }
@@ -244,13 +277,16 @@ ngx_win32_rename_file(ngx_str_t *from, ngx_str_t *to, ngx_log_t *log)
 ngx_int_t
 ngx_file_info(u_char *file, ngx_file_info_t *sb)
 {
+    size_t                      len;
     long                        rc;
     u_short                    *u;
     ngx_err_t                   err;
     WIN32_FILE_ATTRIBUTE_DATA   fa;
     u_short                     utf16[NGX_UTF16_BUFLEN];
 
-    u = ngx_utf8_to_utf16(utf16, file, NGX_UTF16_BUFLEN);
+    len = NGX_UTF16_BUFLEN;
+
+    u = ngx_utf8_to_utf16(utf16, file, &len);
 
     if (u == NULL) {
         return NGX_FILE_ERROR;
@@ -511,7 +547,7 @@ ngx_fs_bsize(u_char *name)
 
 
 static u_short *
-ngx_utf8_to_utf16(u_short *utf16, u_char *utf8, size_t len)
+ngx_utf8_to_utf16(u_short *utf16, u_char *utf8, size_t *len)
 {
     u_char    *p;
     u_short   *u, *last;
@@ -519,18 +555,18 @@ ngx_utf8_to_utf16(u_short *utf16, u_char *utf8, size_t len)
 
     p = utf8;
     u = utf16;
-    last = utf16 + len;
+    last = utf16 + *len;
 
     while (u < last) {
 
         if (*p < 0x80) {
-            *u = (u_short) *p;
+            *u++ = (u_short) *p;
 
             if (*p == 0) {
+                *len = u - utf16;
                 return utf16;
             }
 
-            u++;
             p++;
 
             continue;
@@ -554,21 +590,21 @@ ngx_utf8_to_utf16(u_short *utf16, u_char *utf8, size_t len)
         return NULL;
     }
 
-    ngx_memcpy(u, utf16, len * 2);
+    ngx_memcpy(u, utf16, *len * 2);
 
     utf16 = u;
-    u += len;
+    u += *len;
 
     for ( ;; ) {
 
         if (*p < 0x80) {
-            *u = (u_short) *p;
+            *u++ = (u_short) *p;
 
             if (*p == 0) {
+                *len = u - utf16;
                 return utf16;
             }
 
-            u++;
             p++;
 
             continue;

commit 27b52ab178b8ff8bb4f837708e23f0bfda9952e3

compare long file names in case-insensitive mode, the bug had been introduced in r3418
Former-commit-id: f953436886ad8def8eda55d0693f609d8a8b5a89

src/os/win32/ngx_files.c
@@ -184,13 +184,11 @@ name|len
 operator|-
 literal|1
 operator|||
-name|ngx_memcmp
+name|_wcsicmp
 argument_list|(
 name|u
 argument_list|,
 name|lu
-argument_list|,
-name|n
 argument_list|)
 operator|!=
 literal|0

src/os/win32/ngx_win32_config.h
@@ -145,6 +145,12 @@ directive|include
 file|<stdarg.h>
 end_include
 
+begin_include
+include|#
+directive|include
+file|<locale.h>
+end_include
+
 begin_ifdef
 ifdef|#
 directive|ifdef

src/os/win32/ngx_win32_init.c
@@ -99,7 +99,7 @@ decl_stmt|;
 end_decl_stmt
 
 begin_typedef
-DECL|struct|__anon27f14d110108
+DECL|struct|__anon28c0f3da0108
 typedef|typedef
 struct|struct
 block|{
@@ -428,6 +428,14 @@ control|)
 block|{
 comment|/* void */
 block|}
+comment|/* delete default "C" locale for _wcsicmp() */
+name|setlocale
+argument_list|(
+name|LC_ALL
+argument_list|,
+literal|""
+argument_list|)
+expr_stmt|;
 comment|/* init Winsock */
 if|if
 condition|(