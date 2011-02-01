dmgerman/nginx

commit d9faa8a76194e703a648541b1fa2a117e181e4ee

allow override redirect status in error_page
git-svn-id: svn://svn.nginx.org/nginx/trunk@3782 c3fe7df1-7212-e011-8a91-001109144009

src/http/ngx_http_special_response.c
@@ -523,7 +523,9 @@ ngx_http_send_error_page(ngx_http_request_t *r, ngx_http_err_page_t *err_page)
         r->expect_tested = 1;
     }
 
-    r->err_status = overwrite;
+    if (overwrite >= 0) {
+        r->err_status = overwrite;
+    }
 
     if (ngx_http_complex_value(r, &err_page->value, &uri) != NGX_OK) {
         return NGX_ERROR;
@@ -556,7 +558,7 @@ ngx_http_send_error_page(ngx_http_request_t *r, ngx_http_err_page_t *err_page)
         return NGX_ERROR;
     }
 
-    r->err_status = NGX_HTTP_MOVED_TEMPORARILY;
+    r->err_status = overwrite > 0 ? overwrite : NGX_HTTP_MOVED_TEMPORARILY;
 
     location->hash = 1;
     ngx_str_set(&location->key, "Location");
@@ -570,7 +572,7 @@ ngx_http_send_error_page(ngx_http_request_t *r, ngx_http_err_page_t *err_page)
         return ngx_http_send_refresh(r);
     }
 
-    return ngx_http_send_special_response(r, clcf, NGX_HTTP_MOVED_TEMPORARILY
+    return ngx_http_send_special_response(r, clcf, r->err_status
                                                    - NGX_HTTP_MOVED_PERMANENTLY
                                                    + NGX_HTTP_LEVEL_200);
 }

src/http/ngx_http_core_module.c
@@ -4069,19 +4069,15 @@ ngx_http_core_error_page(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
             return NGX_CONF_ERROR;
         }
 
-        if (overwrite >= 0) {
-            err->overwrite = overwrite;
+        err->overwrite = overwrite;
 
-        } else {
+        if (overwrite == -1) {
             switch (err->status) {
                 case NGX_HTTP_TO_HTTPS:
                 case NGX_HTTPS_CERT_ERROR:
                 case NGX_HTTPS_NO_CERT:
                     err->overwrite = NGX_HTTP_BAD_REQUEST;
-                    break;
-
                 default:
-                    err->overwrite = err->status;
                     break;
             }
         }

commit 0590cca8aa03235746196cd62ab5c051e804edcd

fix "error_page 497 https://" case patch by Maxim Dounin the bug has been introduced in r3782
Former-commit-id: 64d5f9ba268f48170ac6a0d188d934de442072fe

src/http/ngx_http_special_response.c
@@ -1783,18 +1783,33 @@ return|return
 name|NGX_ERROR
 return|;
 block|}
+if|if
+condition|(
+name|overwrite
+operator|>=
+name|NGX_HTTP_MOVED_PERMANENTLY
+operator|&&
+name|overwrite
+operator|<=
+name|NGX_HTTP_SEE_OTHER
+condition|)
+block|{
 name|r
 operator|->
 name|err_status
 operator|=
 name|overwrite
-operator|>
-literal|0
-condition|?
-name|overwrite
-else|:
+expr_stmt|;
+block|}
+else|else
+block|{
+name|r
+operator|->
+name|err_status
+operator|=
 name|NGX_HTTP_MOVED_TEMPORARILY
 expr_stmt|;
+block|}
 name|location
 operator|->
 name|hash