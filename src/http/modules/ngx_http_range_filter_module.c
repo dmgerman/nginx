ddf7379601270fb6464e5e2c46f008f366023340
Igor Sysoev <igor@sysoev.ru>
2011-09-01 13:03:55 +0000

    Now if client requests more ranges than "max_ranges" permits, nginx disables ranges and returns just the source response.
    Former-commit-id: 09be2f18c55ac403cbc9103f47ea5e5c9e8604fa

--- src/http/modules/ngx_http_range_filter_module.c
+++ src/http/modules/ngx_http_range_filter_module.c
@@ -26,7 +26,7 @@ comment|/*  * the single part format:  *  * "HTTP/1.0 206 Partial Content" CRLF 
 end_comment
 
 begin_typedef
-DECL|struct|__anon2c6ee5870108
+DECL|struct|__anon29e2ddf10108
 typedef|typedef
 struct|struct
 block|{
@@ -49,7 +49,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon2c6ee5870208
+DECL|struct|__anon29e2ddf10208
 typedef|typedef
 struct|struct
 block|{
@@ -1211,13 +1211,15 @@ name|start
 expr_stmt|;
 if|if
 condition|(
-operator|--
 name|ranges
+operator|--
 operator|==
 literal|0
 condition|)
 block|{
-break|break;
+return|return
+name|NGX_DECLINED
+return|;
 block|}
 block|}
 if|if
