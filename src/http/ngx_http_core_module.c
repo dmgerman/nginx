dmgerman/nginx

commit c67c34e31b9a4dcd0e95dbd47db3d980fbc03e6f

Fix of "keepalive_disable" directive.
git-svn-id: svn://svn.nginx.org/nginx/trunk@4267 73f98a42-aea0-e011-b76d-00259023448c

src/http/ngx_http_core_module.c
@@ -143,7 +143,7 @@ static ngx_conf_enum_t  ngx_http_core_if_modified_since[] = {
 };
 
 
-static ngx_conf_enum_t  ngx_http_core_keepalive_disable[] = {
+static ngx_conf_bitmask_t  ngx_http_core_keepalive_disable[] = {
     { ngx_string("none"), NGX_HTTP_KEEPALIVE_DISABLE_NONE },
     { ngx_string("msie6"), NGX_HTTP_KEEPALIVE_DISABLE_MSIE6 },
     { ngx_string("safari"), NGX_HTTP_KEEPALIVE_DISABLE_SAFARI },
@@ -513,8 +513,8 @@ static ngx_command_t  ngx_http_core_commands[] = {
       NULL },
 
     { ngx_string("keepalive_disable"),
-      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1,
-      ngx_conf_set_enum_slot,
+      NGX_HTTP_MAIN_CONF|NGX_HTTP_SRV_CONF|NGX_HTTP_LOC_CONF|NGX_CONF_TAKE12,
+      ngx_conf_set_bitmask_slot,
       NGX_HTTP_LOC_CONF_OFFSET,
       offsetof(ngx_http_core_loc_conf_t, keepalive_disable),
       &ngx_http_core_keepalive_disable },
@@ -3475,9 +3475,11 @@ ngx_http_core_merge_loc_conf(ngx_conf_t *cf, void *parent, void *child)
     ngx_conf_merge_msec_value(conf->client_body_timeout,
                               prev->client_body_timeout, 60000);
 
-    ngx_conf_merge_uint_value(conf->keepalive_disable, prev->keepalive_disable,
-                              NGX_HTTP_KEEPALIVE_DISABLE_MSIE6
-                              |NGX_HTTP_KEEPALIVE_DISABLE_SAFARI);
+    ngx_conf_merge_bitmask_value(conf->keepalive_disable,
+                              prev->keepalive_disable,
+                              (NGX_CONF_BITMASK_SET
+                               |NGX_HTTP_KEEPALIVE_DISABLE_MSIE6
+                               |NGX_HTTP_KEEPALIVE_DISABLE_SAFARI));
     ngx_conf_merge_uint_value(conf->satisfy, prev->satisfy,
                               NGX_HTTP_SATISFY_ALL);
     ngx_conf_merge_uint_value(conf->if_modified_since, prev->if_modified_since,

commit 010b4d62a4dc55c88cdb88f7a1993bc975899f23

Fixed "keepalive_disable".
Patch by Alexander Usov.  The bug has been introduced in r4267.


Former-commit-id: 1725621e8a86aafb2db836c8581ea6998e118499

src/http/ngx_http_core_module.c
@@ -22,7 +22,7 @@ file|<ngx_http.h>
 end_include
 
 begin_typedef
-DECL|struct|__anon293f50680108
+DECL|struct|__anon2ba945a60108
 typedef|typedef
 struct|struct
 block|{
@@ -13916,7 +13916,7 @@ return|return
 name|NULL
 return|;
 block|}
-comment|/*      * set by ngx_pcalloc():      *      *     clcf->root = { 0, NULL };      *     clcf->limit_except = 0;      *     clcf->post_action = { 0, NULL };      *     clcf->types = NULL;      *     clcf->default_type = { 0, NULL };      *     clcf->error_log = NULL;      *     clcf->error_pages = NULL;      *     clcf->try_files = NULL;      *     clcf->client_body_path = NULL;      *     clcf->regex = NULL;      *     clcf->exact_match = 0;      *     clcf->auto_redirect = 0;      *     clcf->alias = 0;      *     clcf->gzip_proxied = 0;      */
+comment|/*      * set by ngx_pcalloc():      *      *     clcf->root = { 0, NULL };      *     clcf->limit_except = 0;      *     clcf->post_action = { 0, NULL };      *     clcf->types = NULL;      *     clcf->default_type = { 0, NULL };      *     clcf->error_log = NULL;      *     clcf->error_pages = NULL;      *     clcf->try_files = NULL;      *     clcf->client_body_path = NULL;      *     clcf->regex = NULL;      *     clcf->exact_match = 0;      *     clcf->auto_redirect = 0;      *     clcf->alias = 0;      *     clcf->gzip_proxied = 0;      *     clcf->keepalive_disable = 0;      */
 name|clcf
 operator|->
 name|client_max_body_size
@@ -13934,12 +13934,6 @@ operator|->
 name|client_body_timeout
 operator|=
 name|NGX_CONF_UNSET_MSEC
-expr_stmt|;
-name|clcf
-operator|->
-name|keepalive_disable
-operator|=
-name|NGX_CONF_UNSET_UINT
 expr_stmt|;
 name|clcf
 operator|->