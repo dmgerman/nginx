dmgerman/nginx

commit e733c30553e4d7d01c6527e9a396c6148a05cf0b

"down" server caused endless loop
git-svn-id: svn://svn.nginx.org/nginx/trunk@1377 c3fe7df1-7212-e011-8a91-001109144009

src/http/ngx_http_upstream_round_robin.c
@@ -48,11 +48,11 @@ ngx_http_upstream_init_round_robin(ngx_conf_t *cf,
                 peers->peer[n].sockaddr = server[i].addrs[j].sockaddr;
                 peers->peer[n].socklen = server[i].addrs[j].socklen;
                 peers->peer[n].name = server[i].addrs[j].name;
-                peers->peer[n].weight = server[i].weight;
-                peers->peer[n].current_weight = server[i].weight;
                 peers->peer[n].max_fails = server[i].max_fails;
                 peers->peer[n].fail_timeout = server[i].fail_timeout;
                 peers->peer[n].down = server[i].down;
+                peers->peer[n].weight = server[i].down ? 0 : server[i].weight;
+                peers->peer[n].current_weight = peers->peer[n].weight;
                 n++;
             }
         }

commit 23e9c3127838a873737fcd1fa2aaa07afcbf504b

Upstream: fixed ip_hash rebalancing with the "down" flag.
Due to weight being set to 0 for down peers, order of peers after sorting
wasn't the same as without the "down" flag (with down peers at the end),
resulting in client rebalancing for clients on other servers.  The only
rebalancing which should happen after adding "down" to a server is one
for clients on the server.

The problem was introduced in r1377 (which fixed endless loop by setting
weight to 0 for down servers).  The loop is no longer possible with new
smooth algorithm, so preserving original weight is safe.


Former-commit-id: 4d5759e09841676dc745012f7e551e83e3b02fee

src/http/ngx_http_upstream_round_robin.c
@@ -475,15 +475,6 @@ index|[
 name|i
 index|]
 operator|.
-name|down
-condition|?
-literal|0
-else|:
-name|server
-index|[
-name|i
-index|]
-operator|.
 name|weight
 expr_stmt|;
 name|peers
@@ -495,11 +486,9 @@ index|]
 operator|.
 name|effective_weight
 operator|=
-name|peers
-operator|->
-name|peer
+name|server
 index|[
-name|n
+name|i
 index|]
 operator|.
 name|weight