7af7f51cf1a8ea562e26112bb5ee9e0657c4eb95
Ruslan Ermilov <ru@nginx.com>
2012-05-16 13:09:39 +0000

    Version bump.
    Former-commit-id: 6888516b2bb4f9cce73184744a2ac5b44ce6752a

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|1003000
+value|1003001
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"1.3.0"
+value|"1.3.1"
 end_define
 
 begin_define
