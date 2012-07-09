218528a88b227da0b93d675dfe5410e6c93dd5c8
Maxim Dounin <mdounin@mdounin.ru>
2012-07-09 17:04:37 +0000

    Entity tags: empty etags handling in If-Range.
    Entity tag may be of length 2 as per RFC 2616, i.e. double quotes only.
    Pointed out by Ruslan Ermilov.
    
    
    Former-commit-id: e3acbc6bbbd69cb38de2b19847db6025fbdc2e1d

--- src/http/modules/ngx_http_range_filter_module.c
+++ src/http/modules/ngx_http_range_filter_module.c
@@ -26,7 +26,7 @@ comment|/*  * the single part format:  *  * "HTTP/1.0 206 Partial Content" CRLF 
 end_comment
 
 begin_typedef
-DECL|struct|__anon2ba5d6720108
+DECL|struct|__anon289a59620108
 typedef|typedef
 struct|struct
 block|{
@@ -49,7 +49,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon2ba5d6720208
+DECL|struct|__anon289a59620208
 typedef|typedef
 struct|struct
 block|{
@@ -565,7 +565,7 @@ condition|(
 name|if_range
 operator|->
 name|len
-operator|>
+operator|>=
 literal|2
 operator|&&
 name|if_range
