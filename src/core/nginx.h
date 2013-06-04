e8d290402738edeac3e40eb18c3b50efb34ce95d
Maxim Dounin <mdounin@mdounin.ru>
2013-06-04 19:38:20 +0400

    Version bump.
    Former-commit-id: d37a0a2c7a2570f4108513c09da818985c85b6f4

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|1005001
+value|1005002
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"1.5.1"
+value|"1.5.2"
 end_define
 
 begin_define
