d06bc6419de6366b6c01f2d7bc2bcf1677ef5f0a
Maxim Dounin <mdounin@mdounin.ru>
2014-03-31 22:47:42 +0400

    Adjusted default value of types_hash_bucket_size (ticket #352).
    The ngx_cacheline_size may be too low on some platforms, resulting
    in unexpected hash build problems (as no collisions are tolerated due
    to low bucket_size, and max_size isn't big enough to build a hash without
    collisions).  These problems aren't fatal anymore but nevertheless
    need to be addressed.
    
    
    Former-commit-id: 2349a6747d48a48cebcc5227f82083c33a799517

--- src/http/ngx_http_core_module.c
+++ src/http/ngx_http_core_module.c
@@ -22,7 +22,7 @@ file|<ngx_http.h>
 end_include
 
 begin_typedef
-DECL|struct|__anon29e1295f0108
+DECL|struct|__anon2bc985fc0108
 typedef|typedef
 struct|struct
 block|{
@@ -15923,7 +15923,7 @@ name|prev
 operator|->
 name|types_hash_bucket_size
 argument_list|,
-name|ngx_cacheline_size
+literal|64
 argument_list|)
 expr_stmt|;
 name|conf
