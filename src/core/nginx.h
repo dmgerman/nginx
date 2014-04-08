3ceec29bbac2d5755bd94b419e9dace0b53ff0b2
Valentin Bartenev <vbart@nginx.com>
2014-04-08 20:11:31 +0400

    Version bump.
    Former-commit-id: 644a43ebd281dbe6e6857cad57b979f8cc3d9e88

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|1005013
+value|1005014
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"1.5.13"
+value|"1.5.14"
 end_define
 
 begin_define
