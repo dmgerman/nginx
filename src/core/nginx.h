f702e9d9627fbd9d040022c81b9844952758f158
Valentin Bartenev <vbart@nginx.com>
2014-10-02 22:36:27 +0400

    Version bump.
    Former-commit-id: 70995077ad9866b6c85d7fbb86d31bc5445bc821

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|1007006
+value|1007007
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"1.7.6"
+value|"1.7.7"
 end_define
 
 begin_define
