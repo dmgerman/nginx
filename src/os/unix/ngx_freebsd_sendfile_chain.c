b45f10d2ae548487582fe414fb3e731dcc88febf
Valentin Bartenev <vbart@nginx.com>
2015-04-06 11:22:24 +0300

    Used the correct type for the AIO preload handler return value.
    Former-commit-id: 1329c4af80c93d5f0157728a009196199a42c472

--- src/os/unix/ngx_freebsd_sendfile_chain.c
+++ src/os/unix/ngx_freebsd_sendfile_chain.c
@@ -891,7 +891,7 @@ operator|=
 literal|0
 expr_stmt|;
 block|}
-name|rc
+name|n
 operator|=
 name|aio
 operator|->
@@ -902,7 +902,7 @@ argument_list|)
 expr_stmt|;
 if|if
 condition|(
-name|rc
+name|n
 operator|>
 literal|0
 condition|)
