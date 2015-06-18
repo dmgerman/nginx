5e1cabb492d39600b254169c534349896c1e5f98
Piotr Sikora <piotr@cloudflare.com>
2015-06-18 03:36:41 -0700

    Stream: avoid SSL_CTX_set_tmp_rsa_callback() call with LibreSSL.
    Signed-off-by: Piotr Sikora <piotr@cloudflare.com>
    
    Former-commit-id: 130396928a3537133c905c562fad83a847d93608

--- src/stream/ngx_stream_ssl_module.c
+++ src/stream/ngx_stream_ssl_module.c
@@ -1081,6 +1081,9 @@ name|SSL_OP_CIPHER_SERVER_PREFERENCE
 argument_list|)
 expr_stmt|;
 block|}
+ifndef|#
+directive|ifndef
+name|LIBRESSL_VERSION_NUMBER
 name|SSL_CTX_set_tmp_rsa_callback
 argument_list|(
 name|conf
@@ -1092,6 +1095,8 @@ argument_list|,
 name|ngx_ssl_rsa512_key_callback
 argument_list|)
 expr_stmt|;
+endif|#
+directive|endif
 if|if
 condition|(
 name|ngx_ssl_dhparam
