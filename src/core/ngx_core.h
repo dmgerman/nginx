9b9e49340d1fd8ec4e927dca88fcc5761f38c6ee
Maxim Dounin <mdounin@mdounin.ru>
2016-02-04 18:30:21 +0300

    Dynamic modules: moved module-related stuff to separate files.
    Former-commit-id: 798833457018ee8a6274ec78e816f747361c0d47

--- src/core/ngx_core.h
+++ src/core/ngx_core.h
@@ -491,6 +491,12 @@ end_include
 begin_include
 include|#
 directive|include
+file|<ngx_module.h>
+end_include
+
+begin_include
+include|#
+directive|include
 file|<ngx_open_file_cache.h>
 end_include
 
