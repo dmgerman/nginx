037b3f8ccb765fd80b5dd28744d94feede002929
Vladimir Homutov <vl@nginx.com>
2016-07-26 19:07:18 +0300

    Version bump.
    Former-commit-id: 386e3e80bbf0507ae3ea101b32b19460dd5e3553

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|1011003
+value|1011004
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"1.11.3"
+value|"1.11.4"
 end_define
 
 begin_define
