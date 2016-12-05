0c8130aa34a20b14dd66ddacaa772c75c546cf33
Maxim Dounin <mdounin@mdounin.ru>
2016-12-05 22:23:23 +0300

    SSL: $ssl_curves (ticket #1088).
    The variable contains a list of curves as supported by the client.
    Known curves are listed by their names, unknown ones are shown
    in hex, e.g., "0x001d:prime256v1:secp521r1:secp384r1".
    
    Note that OpenSSL uses session data for SSL_get1_curves(), and
    it doesn't store full list of curves supported by the client when
    serializing a session.  As a result $ssl_curves is only available
    for new sessions (and will be empty for reused ones).
    
    The variable is only meaningful when using OpenSSL 1.0.2 and above.
    With older versions the variable is empty.
    
    
    Former-commit-id: 551091951a479e2f512062c51bdcc6157a211164

--- src/event/ngx_event_openssl.h
+++ src/event/ngx_event_openssl.h
@@ -431,7 +431,7 @@ struct|;
 end_struct
 
 begin_typedef
-DECL|struct|__anon29afd9e00108
+DECL|struct|__anon2a475f440108
 typedef|typedef
 struct|struct
 block|{
@@ -460,7 +460,7 @@ name|SSL_CTRL_SET_TLSEXT_TICKET_KEY_CB
 end_ifdef
 
 begin_typedef
-DECL|struct|__anon29afd9e00208
+DECL|struct|__anon2a475f440208
 typedef|typedef
 struct|struct
 block|{
@@ -1066,6 +1066,25 @@ end_function_decl
 begin_function_decl
 name|ngx_int_t
 name|ngx_ssl_get_ciphers
+parameter_list|(
+name|ngx_connection_t
+modifier|*
+name|c
+parameter_list|,
+name|ngx_pool_t
+modifier|*
+name|pool
+parameter_list|,
+name|ngx_str_t
+modifier|*
+name|s
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|ngx_int_t
+name|ngx_ssl_get_curves
 parameter_list|(
 name|ngx_connection_t
 modifier|*
