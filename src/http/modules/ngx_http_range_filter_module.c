f86d66547f2b9240e28fc69aed1f69409237c638
Maxim Dounin <mdounin@mdounin.ru>
2017-06-27 00:53:46 +0300

    Range filter: allowed ranges on empty files (ticket #1031).
    As per RFC 2616 / RFC 7233, any range request to an empty file
    is expected to result in 416 Range Not Satisfiable response, as
    there cannot be a "byte-range-spec whose first-byte-pos is less
    than the current length of the entity-body".  On the other hand,
    this makes use of byte-range requests inconvenient in some cases,
    as reported for the slice module here:
    
    http://mailman.nginx.org/pipermail/nginx-devel/2017-June/010177.html
    
    This commit changes range filter to instead return 200 if the file
    is empty and the range requested starts at 0.
    
    
    Former-commit-id: 774f179a9b523cff2233846283dc35a5582aa1d1

--- src/http/modules/ngx_http_range_filter_module.c
+++ src/http/modules/ngx_http_range_filter_module.c
@@ -26,7 +26,7 @@ comment|/*  * the single part format:  *  * "HTTP/1.0 206 Partial Content" CRLF 
 end_comment
 
 begin_typedef
-DECL|struct|__anon27612bc10108
+DECL|struct|__anon2c4b62ca0108
 typedef|typedef
 struct|struct
 block|{
@@ -49,7 +49,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon27612bc10208
+DECL|struct|__anon2c4b62ca0208
 typedef|typedef
 struct|struct
 block|{
@@ -1478,6 +1478,17 @@ return|return
 name|NGX_DECLINED
 return|;
 block|}
+block|}
+if|else if
+condition|(
+name|start
+operator|==
+literal|0
+condition|)
+block|{
+return|return
+name|NGX_DECLINED
+return|;
 block|}
 if|if
 condition|(
