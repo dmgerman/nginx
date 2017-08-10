999c5c30c65dcef9d2fb004eed93c77905bd1ef2
Maxim Dounin <mdounin@mdounin.ru>
2017-08-10 22:21:22 +0300

    Restored ngx_event_aio_t layout for debug logging.
    The "fd" field should be after 3 pointers for ngx_event_ident() to use it.
    This was broken by ccad84a174e0.  While it does not seem to be currently used
    for aio-related events, it should be a good idea to preserve the correct
    layout nevertheless.
    
    
    Former-commit-id: dd5ab4a11f5b423482f63c78835c9dddada766fb

--- src/event/ngx_event.h
+++ src/event/ngx_event.h
@@ -45,7 +45,7 @@ operator|)
 end_if
 
 begin_typedef
-DECL|struct|__anon2a31348b0108
+DECL|struct|__anon2795067a0108
 typedef|typedef
 struct|struct
 block|{
@@ -332,6 +332,10 @@ name|ngx_file_t
 modifier|*
 name|file
 decl_stmt|;
+DECL|member|fd
+name|ngx_fd_t
+name|fd
+decl_stmt|;
 if|#
 directive|if
 operator|(
@@ -353,10 +357,6 @@ parameter_list|)
 function_decl|;
 endif|#
 directive|endif
-DECL|member|fd
-name|ngx_fd_t
-name|fd
-decl_stmt|;
 if|#
 directive|if
 operator|(
@@ -406,7 +406,7 @@ directive|endif
 end_endif
 
 begin_typedef
-DECL|struct|__anon2a31348b0208
+DECL|struct|__anon2795067a0208
 typedef|typedef
 struct|struct
 block|{
@@ -1446,7 +1446,7 @@ value|0x02000000
 end_define
 
 begin_typedef
-DECL|struct|__anon2a31348b0308
+DECL|struct|__anon2795067a0308
 typedef|typedef
 struct|struct
 block|{
@@ -1493,7 +1493,7 @@ typedef|;
 end_typedef
 
 begin_typedef
-DECL|struct|__anon2a31348b0408
+DECL|struct|__anon2795067a0408
 typedef|typedef
 struct|struct
 block|{
