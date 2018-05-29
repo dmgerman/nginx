785b1dc8326c71b8c6920729d09f18c67df9b177
Ruslan Ermilov <ru@nginx.com>
2018-05-29 11:47:32 +0300

    Win32: fixed comment in ngx_gettimeofday() calculations.
    Former-commit-id: 4271e48911bca21c8b81f2d8971218a6f1692468

--- src/os/win32/ngx_time.c
+++ src/os/win32/ngx_time.c
@@ -38,7 +38,7 @@ operator|&
 name|ft
 argument_list|)
 expr_stmt|;
-comment|/*      * A file time is a 64-bit value that represents the number      * of 100-nanosecond intervals that have elapsed since      * January 1, 1601 12:00 A.M. UTC.      *      * Between January 1, 1970 (Epoch) and January 1, 1601 there were      * 134744 days,      * 11644473600 seconds or      * 11644473600,000,000,0 100-nanosecond intervals.      *      * See also MSKB Q167296.      */
+comment|/*      * A file time is a 64-bit value that represents the number      * of 100-nanosecond intervals that have elapsed since      * January 1, 1601 12:00 A.M. UTC.      *      * Between January 1, 1970 (Epoch) and January 1, 1601 there were      * 134774 days,      * 11644473600 seconds or      * 11644473600,000,000,0 100-nanosecond intervals.      *      * See also MSKB Q167296.      */
 name|intervals
 operator|=
 operator|(
