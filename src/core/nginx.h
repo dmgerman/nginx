04cd8caefb61dea7f763569e48009448eeffbc75
Sergey Kandaurov <pluknet@nginx.com>
2018-07-24 18:46:18 +0300

    Version bump.
    Former-commit-id: a1e2c5566c628dbf39e3f6c105ddc1dde5b41a58

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|1015002
+value|1015003
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"1.15.2"
+value|"1.15.3"
 end_define
 
 begin_define
