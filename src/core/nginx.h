e6814bffd06c9bc91587dc28362d677f4908817b
Roman Arutyunyan <arut@nginx.com>
2019-05-27 19:47:50 +0300

    Version bump.
    Former-commit-id: e0768d199dcdfe7d78a160d31a95418b20d10941

--- src/core/nginx.h
+++ src/core/nginx.h
@@ -21,7 +21,7 @@ DECL|macro|nginx_version
 define|#
 directive|define
 name|nginx_version
-value|1017000
+value|1017001
 end_define
 
 begin_define
@@ -29,7 +29,7 @@ DECL|macro|NGINX_VERSION
 define|#
 directive|define
 name|NGINX_VERSION
-value|"1.17.0"
+value|"1.17.1"
 end_define
 
 begin_define
