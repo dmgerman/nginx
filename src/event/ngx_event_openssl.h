a832294c341921849212decbd4a504bb69e865d7
Vladimir Homutov <vl@nginx.com>
2021-10-14 11:46:23 +0300

    SSL: added $ssl_alpn_protocol variable.
    The variable contains protocol selected by ALPN during handshake and
    is empty otherwise.
    
    
    Former-commit-id: a9f4f25b72c39653795dfb4b1f13b55625fb9fbc

--- src/event/ngx_event_openssl.h
+++ src/event/ngx_event_openssl.h
@@ -623,7 +623,7 @@ struct|;
 end_struct
 
 begin_typedef
-DECL|struct|__anon2a0dc18a0108
+DECL|struct|__anon2760cab50108
 typedef|typedef
 struct|struct
 block|{
@@ -652,7 +652,7 @@ name|SSL_CTRL_SET_TLSEXT_TICKET_KEY_CB
 end_ifdef
 
 begin_typedef
-DECL|struct|__anon2a0dc18a0208
+DECL|struct|__anon2760cab50208
 typedef|typedef
 struct|struct
 block|{
@@ -1564,6 +1564,25 @@ end_function_decl
 begin_function_decl
 name|ngx_int_t
 name|ngx_ssl_get_server_name
+parameter_list|(
+name|ngx_connection_t
+modifier|*
+name|c
+parameter_list|,
+name|ngx_pool_t
+modifier|*
+name|pool
+parameter_list|,
+name|ngx_str_t
+modifier|*
+name|s
+parameter_list|)
+function_decl|;
+end_function_decl
+
+begin_function_decl
+name|ngx_int_t
+name|ngx_ssl_get_alpn_protocol
 parameter_list|(
 name|ngx_connection_t
 modifier|*
